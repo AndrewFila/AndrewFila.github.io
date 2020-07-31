091cccc007fb5a111adf1f61cf9d87c95bf1abed
FilaAndrew <fila791@gmail.com>
2020-07-31 21:41:00 +0000

    update

---
+++ contact-form.php
@@ -0,0 +1,12 @@
+<?php
+if(isset($_POST(['submit'])) {
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
+    
+    $to = "amfila@mtu.edu";
+    
+    mail();
+}
+
+?>
